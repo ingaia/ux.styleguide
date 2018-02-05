41cef25ddb56d05c4a2724a608a4c9d96eb7ea2f
Thiago Franchin <thiago.franchin@i-value.int>
2018-02-05 17:57:11 -0200

    Buttons, forms, bars progress, badge

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import 'jquery';
 import '../node_modules/ingaia-font-icons/dist/ingaia-font-icons.min.css';
 import createAlert from './js/ingaia-alert';
 window.createAlert = createAlert;
+import 'material-design-lite';
 import 'bootstrap';
